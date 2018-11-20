bfb18d6c267f28b55fd998171e5eddd5252027b5
Leonardo Gomes <lgdevlop@gmail.com>
2018-11-20 23:07:30 +0100

    Incluido testes no recurso /planetas da API

--- src/server/api/planeta.test.js
+++ src/server/api/planeta.test.js
@@ -1,46 +1,19 @@
 const planetaApi = require('./planeta')
 const db = require('../db')
 
-const requisicao = {}
-const resposta = {}
+describe('Testando o recurso /planetas da API', () => {
+  it('Obtem todos os planetas do banco de dados', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
 
-const dados = [
-  { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
-]
-const dadosNovos = [
-  { nome: 'Abacate', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
-]
-// let retornoResposta = ''
-// resposta.json = jest.fn((parametro) => {
-//   // expect(parametro).toBe(dadosNovos)
-//   // retornoResposta = parametro
-//   console.log('Resposta JSON', parametro)
-// })
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
 
-describe('Obtem todos os planetas', () => {
-  it('Obtem todos os planetas do banco de dados', () => {
-    // const requisicao = {}
-    // const resposta = {}
-
-    // const dados = [
-    //   { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
-    // ]
-    // const dadosNovos = [
-    //   { nome: 'Abacate', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
-    // ]
-
-    // resposta.json = (parametro) => {
-    //   expect(parametro).toBe(dadosNovos)
-    // }
-
-    resposta.json = jest.fn((parametro) => {
-      // expect(parametro).toBe(dadosNovos)
-      // retornoResposta = parametro
-      console.log('Resposta JSON', parametro)
-    })
-
-    // const argumento = resposta.json.mock.calls.calls[0][0]
-    // console.log('argumento: ', argumento)
+    resposta.json = (args) => {
+      expect(args).toBe(dados)
+      terminado()
+    }
 
     db.obtemTodosPlanetas = () =>
       new Promise((resolve, reject) => {
@@ -51,118 +24,356 @@ describe('Obtem todos os planetas', () => {
           reject(dados)
         }
       })
-    expect.assertions(1)
     planetaApi.obtemTodosPlanetas(requisicao, resposta)
-    // expect(retornoResposta).toBe(dadosNovos)
-    expect(resposta.json).toHaveBeenCalledWith(dados)
   })
 
-  // it('Obtem uma mensagem de não encontrado do banco de dados', () => {
-  //   const requisicao = {}
-  //   const resposta = {}
+  it('Planeta não encontrado no banco de dados(nenhum planeta cadastrado)', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
 
-  //   resposta.status = (status) => (mensagem) => {
-  //     expect(status).toBe(202)
-  //     expect(mensagem).toBe('Não Encontrado')
-  //   }
+    resposta.send = (mensagem) => {
+      expect(mensagem).toBe('Não Encontrado')
+      terminado()
+    }
 
-  //   db.obtemTodosPlanetas = () =>
-  //     new Promise((resolve, reject) => {
-  //       const erro = { message: 'Não Encontrado' }
-  //       const res = 0
-  //       if (res === 1) {
-  //         resolve(erro)
-  //       } else {
-  //         reject(erro)
-  //       }
-  //     })
-  //   planetaApi.obtemTodosPlanetas(requisicao, resposta)
-  // })
+    resposta.status = (status) => {
+      expect(status).toBe(202)
+      return resposta
+    }
 
-  // it('Obtem uma mensagem de erro 500', () => {
-  //   const requisicao = {}
-  //   const resposta = {}
+    resposta.status.mock = { calls: [] }
 
-  //   resposta.status = (status) => (mensagem) => {
-  //     expect(status).toBe(500)
-  //     expect(mensagem).toBe('Erro 500')
-  //   }
+    db.obtemTodosPlanetas = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Não Encontrado' }
+        const res = 0
+        if (res === 1) {
+          resolve(erro)
+        } else {
+          reject(erro)
+        }
+      })
 
-  //   db.obtemTodosPlanetas = () =>
-  //     new Promise((resolve, reject) => {
-  //       const erro = 'Erro 500'
-  //       const res = 0
-  //       if (res === 1) {
-  //         resolve(erro)
-  //       } else {
-  //         reject(erro)
-  //       }
-  //     })
-  //   planetaApi.obtemTodosPlanetas(requisicao, resposta)
-  // })
+    planetaApi.obtemTodosPlanetas(requisicao, resposta)
+  })
+
+  it('Obtem uma mensagem de erro não esperada do Banco de dados', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    resposta.json = (erro) => {
+      expect(erro).toEqual({ message: 'Erro não previsto' })
+      terminado()
+    }
+
+    resposta.send = (mensagem) => {
+      expect(mensagem).toBe('Não Encontrado')
+      return resposta
+    }
+    resposta.status = (status) => {
+      expect(status).toBe(500)
+      return resposta
+    }
+
+    db.obtemTodosPlanetas = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Erro não previsto' }
+        const res = 0
+        if (res === 1) {
+          resolve(erro)
+        } else {
+          reject(erro)
+        }
+      })
+    planetaApi.obtemTodosPlanetas(requisicao, resposta)
+  })
+
+  it('Obtem o planeta por ID', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
+
+    resposta.json = (parametro) => {
+      expect(parametro).toEqual(dados)
+      terminado()
+    }
+
+    requisicao.params = { id: 11 }
+
+    db.obtemPorNomeOuIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const res = 1
+        if (res === 1) {
+          resolve(dados)
+        } else {
+          reject(dados)
+        }
+      })
+    planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
+  })
+
+  it('Obtem o planeta por nome', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
+
+    resposta.json = (parametro) => {
+      expect(parametro).toEqual(dados)
+      terminado()
+    }
+
+    requisicao.params = { id: 'NomeDoPlaneta' }
+
+    db.obtemPorNomeOuIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const res = 1
+        if (res === 1) {
+          resolve(dados)
+        } else {
+          reject(dados)
+        }
+      })
+    planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
+  })
+
+  it('Planeta não encontrado na pesquisa por ID', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
+
+    resposta.send = (mensagem) => {
+      expect(mensagem).toBe('Não Encontrado')
+      terminado()
+    }
+    resposta.status = (status) => {
+      expect(status).toBe(202)
+      return resposta
+    }
+
+    requisicao.params = { id: 11 }
+
+    db.obtemPorNomeOuIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Não Encontrado' }
+        const res = 0
+        if (res === 1) {
+          resolve(dados)
+        } else {
+          reject(erro)
+        }
+      })
+    planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
+  })
+
+  it('Planeta não encontrado na pesquisa por nome', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
+
+    resposta.send = (mensagem) => {
+      expect(mensagem).toBe('Não Encontrado')
+      terminado()
+    }
+    resposta.status = (status) => {
+      expect(status).toBe(202)
+      return resposta
+    }
+
+    requisicao.params = { id: 'nomeDoPlaneta' }
+
+    db.obtemPorNomeOuIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Não Encontrado' }
+        const res = 0
+        if (res === 1) {
+          resolve(dados)
+        } else {
+          reject(erro)
+        }
+      })
+    planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
+  })
+
+  it('Obtem uma mensagem de erro não esperada do Banco de dados, na pesquisa por nome do planeta', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    resposta.json = (erro) => {
+      expect(erro).toEqual({ message: 'Erro não previsto' })
+      terminado()
+    }
+
+    resposta.status = (status) => {
+      expect(status).toBe(500)
+      return resposta
+    }
+
+    requisicao.params = { id: 'nomeDoPlaneta' }
+
+    db.obtemPorNomeOuIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Erro não previsto' }
+        const res = 0
+        if (res === 1) {
+          resolve(erro)
+        } else {
+          reject(erro)
+        }
+      })
+    planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
+  })
+
+  it('Obtem uma mensagem de erro não esperada do Banco de dados, na pesquisa por ID do planeta', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    resposta.json = (erro) => {
+      expect(erro).toEqual({ message: 'Erro não previsto' })
+      terminado()
+    }
+
+    resposta.status = (status) => {
+      expect(status).toBe(500)
+      return resposta
+    }
+
+    requisicao.params = { id: 11 }
+
+    db.obtemPorNomeOuIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Erro não previsto' }
+        const res = 0
+        if (res === 1) {
+          resolve(erro)
+        } else {
+          reject(erro)
+        }
+      })
+
+    planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
+  })
+
+  it('Remove planeta por ID', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    resposta.send = (mensagem) => {
+      expect(mensagem).toBe('removido')
+      terminado()
+    }
+
+    resposta.status = (status) => {
+      expect(status).toBe(200)
+      return resposta
+    }
+
+    requisicao.params = { id: 11 }
+
+    db.removePorIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'ID não encontrado no Banco de Dados' }
+        const res = 1
+        if (res === 1) {
+          resolve('removido')
+        } else {
+          reject(erro)
+        }
+      })
+
+    planetaApi.removePorIdPlaneta(requisicao, resposta)
+  })
+
+  it('ID do planeta não encontrado no banco de dados ao remover', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
+
+    resposta.send = (mensagem) => {
+      expect(mensagem).toBe('ID não encontrado no Banco de Dados')
+      terminado()
+    }
+
+    resposta.status = (status) => {
+      expect(status).toBe(202)
+      return resposta
+    }
+
+    requisicao.params = { id: 11 }
+
+    db.removePorIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'ID não encontrado no Banco de Dados' }
+        const res = 0
+        if (res === 1) {
+          resolve(dados)
+        } else {
+          reject(erro)
+        }
+      })
+
+    planetaApi.removePorIdPlaneta(requisicao, resposta)
+  })
+
+  it('Erro no banco de dados ao remover', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    const dados = [
+      { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
+    ]
+
+    resposta.json = (parametro) => {
+      expect(parametro).toEqual({ message: 'Erro no Banco de Dados' })
+      terminado()
+    }
+
+    resposta.status = (status) => {
+      expect(status).toBe(500)
+      return resposta
+    }
+
+    requisicao.params = { id: 11 }
+
+    db.removePorIdPlaneta = () =>
+      new Promise((resolve, reject) => {
+        const erro = { message: 'Erro no Banco de Dados' }
+        const res = 0
+        if (res === 1) {
+          resolve(dados)
+        } else {
+          reject(erro)
+        }
+      })
+
+    planetaApi.removePorIdPlaneta(requisicao, resposta)
+  })
+
+  it('ID inválido do planeta informado para remover', (terminado) => {
+    const requisicao = {}
+    const resposta = {}
+
+    resposta.sendStatus = (parametro) => {
+      expect(parametro).toBe(400)
+      terminado()
+    }
+
+    requisicao.params = { id: 'a' }
+
+    planetaApi.removePorIdPlaneta(requisicao, resposta)
+  })
 })
-
-// describe('Obtem o planeta por ID ou por nome', () => {
-//   it('Obtem o planeta por ID', () => {
-//     const requisicao = {}
-//     const resposta = {}
-
-//     const dados = [
-//       { nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 },
-//     ]
-
-//     resposta.json = (parametro) => {
-//       expect(parametro).toBe(dados)
-//     }
-
-//     requisicao.params = { id: 11 }
-
-//     // resposta.status = (status) => (mensagem) => {
-//     //   expect(status).toBe(500)
-//     //   expect(mensagem).toBe('Erro 500')
-//     // }
-
-//     db.obtemPorNomeOuIdPlaneta = (parametro) =>
-//       new Promise((resolve, reject) => {
-//         // const res = 1
-//         if (parametro === requisicao.params.id) {
-//           resolve(dados)
-//         } else {
-//           reject(dados)
-//         }
-//       })
-//     planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
-//   })
-
-//   it('Não encontra o planeta por ID', () => {
-//     const requisicao = {}
-//     const resposta = {}
-//     const dadosErrado = {}
-
-//     // const dados = [{ nome: 'Geonosis', clima: 'Ameno', terreno: 'Lava', aparicoes: 1, id: 11 }]
-
-//     // resposta.json = (parametro) => {
-//     //   expect(parametro).toBe(dados)
-//     // }
-//     resposta.status = (status) => (mensagem) => {
-//       expect(status).toBe(202)
-//       console.log(mensagem)
-//       expect(mensagem).toBe(1) // Errado, rever o mock
-//     }
-
-//     requisicao.params = { id: 11 }
-//     dadosErrado.params = { id: 1 }
-
-//     db.obtemPorNomeOuIdPlaneta = (parametro) =>
-//       new Promise((resolve, reject) => {
-//         // const res = 1
-//         const erro = { message: 'Não Encontrado' }
-//         if (parametro === dadosErrado.params.id) {
-//           resolve(erro)
-//         } else {
-//           reject(erro)
-//         }
-//       })
-//     planetaApi.obtemPorNomeOuIdPlaneta(requisicao, resposta)
-//   })
-// })
